666a60f0683cc41615197ee3b5da4375f5ab82eb
Dale Holstein <DaleHolstein@Dales-MacBook-Pro.local>
2018-02-02 21:57:08 -0500

    added config for mlab

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes);
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytarticles");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 // Start the API server
 app.listen(PORT, () =>
