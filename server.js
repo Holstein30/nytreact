17df538f7bb624538c59816b73b08f920e57f070
Dale Holstein <DaleHolstein@wifi_stu-10-38-72-152.uncc.edu>
2018-02-06 18:29:10 -0500

    maybe fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Set up promises with mongoose
-mongoose.Promise = global.Promise;
+mongoose.Promise = Promise;
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
